b6ecb45a4937c170802afb0be780354641b2a5d6
Mo <Mo@Mos-MacBook-Air.local>
2018-11-19 15:27:01 +0100

    Tokens are now stored in the database, and i made changes in the database so email and tokens are unique - this makes the performance better so that no user can have the same email

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -1,9 +1,11 @@
 package controllers;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicReference;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
@@ -11,10 +13,10 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.mysql.cj.jdbc.exceptions.SQLError;
 import model.User;
 import utils.Hashing;
 import utils.Log;
-
 
 
 public class UserController {
@@ -160,10 +162,10 @@ public class UserController {
       }
 
       // Build the query for DB - can now take stored passwords which are hashed and start data which aren't hashed
-      String sql = "SELECT * FROM cbsexam.user where email ='" + user.getEmail() + "' AND (password = '" + Hashing.sha(user.getPassword()) + "' OR password = '" + user.getPassword() + "')";
+      String sql1 = "SELECT * FROM cbsexam.user where email ='" + user.getEmail() + "' AND (password = '" + Hashing.sha(user.getPassword()) + "' OR password = '" + user.getPassword() + "')";
 
       // Actually do the query
-      ResultSet rs = dbCon.query(sql);
+      ResultSet rs = dbCon.query(sql1);
       User loginUser = null;
 
       try {
@@ -190,17 +192,31 @@ public class UserController {
               }
 
               // Verifying the token
-
+/*
 
               try {
                   Algorithm algorithm = Algorithm.HMAC256("secret");
-                  JWTVerifier verifier = JWT.require(algorithm) .withIssuer("auth0").build(); //Reusable verifier instance
+                  JWTVerifier verifier = JWT.require(algorithm).withIssuer("auth0").build(); //Reusable verifier instance
                   DecodedJWT jwt = verifier.verify(token);
               } catch (JWTVerificationException exception1){
                   //Invalid signature/claims
 
                   System.out.println("Something went wrong with verifying the token - " + exception1.getMessage());
               }
+*/
+
+              try {
+
+
+                      String sql3 = "INSERT INTO tokens (user_id, token) VALUES ('" + loginUser.getId() + "', '" + token + "')";
+
+                      dbCon.insert(sql3);
+
+              }catch (Exception ex){
+
+
+              }
+
 
               // Return the token directly
               return token;
@@ -214,7 +230,6 @@ public class UserController {
 
       // Return null
       return null;
-
 
   }
 
